f5a2b1ef62e26cbe76b44974beb2d597ee89587f
Dalton-Baird <dltnbaird@gmail.com>
2016-09-25 05:40:21 -0700

    Added the solution and projects.

---
+++ ProjectPiBoy.SDLApp/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using static SDL2.SDL;
+using static SDL2.SDL_ttf;
+namespace ProjectPiBoy.SDLApp
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadKey();
+        }
+    }
+}
